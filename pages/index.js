7af49bb939c6720f412ad3e8670ea7c668210177
datacris <datacristian1523@gmail.com>
2021-05-05 17:23:19 -0500

    results and thumbnails

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
+  
   return (
     <div >
       <Head>
@@ -12,8 +16,23 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* nav */}
-      {/* results  */}
+      <Nav />
+      <Results results={results}/>
+
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  };
 }
